7672bbac3ddcc6fdcb0c926b7a7d936b963db2b6
ql17774641928 <2722937531@qq.com>
2024-03-25 16:36:48 +0800

    开始编写Message组件

---
+++ src/components/Message/Message.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div
+    class="vk-messsage"
+    role="alert"
+    >
+   </div>        
+ </template>
+<script setup lang='ts'>
+   import type { MessageOptions } from 'child_process';    
+const props = withDefaults(defineProps<MessageOptions>(),{
+    type:'info',
+    duration:3000
+})      
+</script>
+<style scoped>
+             
+</style>
