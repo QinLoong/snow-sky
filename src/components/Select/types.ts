4edf4ead44b61cb7f578748875134503f221091f
ql17774641928 <2722937531@qq.com>
2024-03-31 14:39:39 +0800

    开始编写select组件

---
+++ src/components/Select/types.ts
@@ -0,0 +1,25 @@
+export interface SelectOption {
+  label: string;
+  value: string;
+  disabled?: boolean;
+}
+
+export interface SelectProps {
+  // v-model
+  modelValue: string;
+  // 选项
+  options: SelectOption[];
+  // 一些基本表单属性
+  placeholder: string;
+  disabled: boolean;
+}
+export interface SelectStates {
+  inputValue: string;
+  selectedOption: null | SelectOption;
+}
+export interface SelectEmits {
+  (e: "change", value: string): void;
+  (e: "update:modelValue", value: string): void;
+  /**代表现在是打开还是关闭*/
+  (e: "visible-change", value: boolean): void;
+}
